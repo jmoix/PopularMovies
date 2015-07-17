509101da6a3e6e678992abd0663e00ef33d80e3b
Jason Moix <jmoix@privacystar.com>
2015-07-17 15:04:10 -0500

    format movie detail screen

--- app/src/main/java/com/jasonmoix/popularmovies/DetailActivity.java
+++ app/src/main/java/com/jasonmoix/popularmovies/DetailActivity.java
@@ -1,16 +1,26 @@
 package com.jasonmoix.popularmovies;
 
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.NavUtils;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -22,10 +32,14 @@ import com.squareup.picasso.Picasso;
  */
 public class DetailActivity extends AppCompatActivity {
 
+    private final int NUM_PAGES = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
+
+        CoordinatorLayout rootLayout = (CoordinatorLayout)findViewById(R.id.main_content);
 
         Log.d("PopularMovies", getIntent().getStringExtra(MoviesContract.MovieEntry.COLUMN_TITLE));
 
@@ -37,21 +51,15 @@ public class DetailActivity extends AppCompatActivity {
                 (CollapsingToolbarLayout)findViewById(R.id.collapsing_toolbar);
         collapsingToolbarLayout.setTitle(getIntent().getStringExtra(MoviesContract.MovieEntry.COLUMN_TITLE));
 
+
         ImageView backdrop = (ImageView)findViewById(R.id.backdrop);
         Uri imagePath = Uri.parse(getString(R.string.base_moviebackdrop_url, getIntent().getStringExtra(MoviesContract.MovieEntry.COLUMN_BACKDROP_PATH)));
         Glide.with(this).load(imagePath).centerCrop().into(backdrop);
 
         Log.d("PopularMovies", getString(R.string.base_moviebackdrop_url, getIntent().getStringExtra(MoviesContract.MovieEntry.COLUMN_BACKDROP_PATH)));
 
-        FragmentManager fm = getSupportFragmentManager();
-        MovieDetailFragment detailFragment = (MovieDetailFragment)fm.findFragmentById(R.id.movie_detail_container);
-        if(detailFragment == null){
-            detailFragment = new MovieDetailFragment();
-            detailFragment.setArguments(getIntent().getExtras());
-            fm.beginTransaction()
-                    .add(R.id.movie_detail_container, detailFragment)
-                    .commit();
-        }
+        ViewPager viewPager = (ViewPager)findViewById(R.id.detailPager);
+        viewPager.setAdapter(new FragmentPager(getSupportFragmentManager()));
 
     }
 
@@ -68,6 +76,22 @@ public class DetailActivity extends AppCompatActivity {
                 finish();
         }
         return super.onOptionsItemSelected(item);
+
+    }
+
+    public class FragmentPager extends FragmentPagerAdapter {
+
+        public FragmentPager(FragmentManager fm){ super(fm); }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return MovieDetailFragment.newInstance(getIntent().getExtras());
+        }
 
     }
 }
--- app/src/main/java/com/jasonmoix/popularmovies/MovieDetailFragment.java
+++ app/src/main/java/com/jasonmoix/popularmovies/MovieDetailFragment.java
@@ -1,23 +1,79 @@
 package com.jasonmoix.popularmovies;
 
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.jasonmoix.popularmovies.data.MoviesContract;
+import com.squareup.picasso.Picasso;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  * Created by jmoix on 7/15/2015.
  */
 public class MovieDetailFragment extends Fragment {
 
+    View rootView;
+
+    public static MovieDetailFragment newInstance(Bundle arguments){
+        MovieDetailFragment fragment = new MovieDetailFragment();
+        fragment.setArguments(arguments);
+        return(fragment);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_movie_detail, container, false);
-        return(view);
+        rootView = inflater.inflate(R.layout.fragment_movie_detail, container, false);
+        return(rootView);
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Picasso.with(getActivity().getBaseContext())
+                .load(getString(R.string.base_movieposter_url, getArguments().getString(MoviesContract.MovieEntry.COLUMN_POSTER_PATH)))
+                .into(((ImageView) rootView.findViewById(R.id.movie_poster)));
+
+        ((TextView)rootView.findViewById(R.id.movie_title)).setText(getArguments().getString(MoviesContract.MovieEntry.COLUMN_TITLE));
+        ((TextView)rootView.findViewById(R.id.movie_release_date)).setText(getString(R.string.release_date,
+                formatDate(getArguments().getString(MoviesContract.MovieEntry.COLUMN_RELEASE_DATE))));
+        ((TextView)rootView.findViewById(R.id.movie_rating)).setText(getString(R.string.movie_rating, getArguments().getString(MoviesContract.MovieEntry.COLUMN_VOTE_AVERAGE)));
+        ((TextView)rootView.findViewById(R.id.movie_overview)).setText(getArguments().getString(MoviesContract.MovieEntry.COLUMN_OVERVIEW));
+
+    }
+
+    public String formatDate(String date){
+
+        Calendar newDate = new GregorianCalendar(Calendar.getInstance().getTimeZone());
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-mm-dd");
+
+        try{
+            Date oldDate = formatter.parse(date);
+            newDate.setTime(oldDate);
+        }catch (ParseException e){
+            e.printStackTrace();
+        }
+
+        return  (newDate.get(Calendar.MONTH)+1) + "/" +
+                newDate.get(Calendar.DAY_OF_MONTH) + "/" +
+                newDate.get(Calendar.YEAR);
 
     }
 }
