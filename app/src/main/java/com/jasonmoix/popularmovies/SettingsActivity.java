3a24f59aa92c6055b7e06d0d2df8caacc6134882
Jason Moix <jmoix@privacystar.com>
2015-07-17 15:59:02 -0500

    add Settings Activity

--- app/src/main/java/com/jasonmoix/popularmovies/SettingsActivity.java
+++ app/src/main/java/com/jasonmoix/popularmovies/SettingsActivity.java
@@ -1,12 +1,20 @@
 package com.jasonmoix.popularmovies;
 
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 /**
  * Created by jmoix on 7/15/2015.
  */
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +22,55 @@ public class SettingsActivity extends PreferenceActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle(getString(R.string.title_activity_settings));
+        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_chevron_left_white_24dp));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SettingsActivity.this.finish();
+            }
+        });
+
+        addPreferencesFromResource(R.xml.pref_general);
+
     }
 
+    private void bindPreferenceSummaryToValue(Preference preference){
+
+        preference.setOnPreferenceChangeListener(this);
+
+        onPreferenceChange(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_order_key)));
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+
+        String stringValue = newValue.toString();
+
+        if(preference instanceof ListPreference){
+
+            ListPreference listPreference = (ListPreference)preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if(prefIndex >= 0){
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else {
+            preference.setSummary(stringValue);
+        }
+
+        return(true);
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public Intent getParentActivityIntent(){
+        return super.getParentActivityIntent().addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    }
 }
