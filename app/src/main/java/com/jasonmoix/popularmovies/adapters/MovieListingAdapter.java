97a38e3c69275a00ef685296fc15f957ae1898f9
Jason Moix <jmoix@privacystar.com>
2015-08-04 15:49:13 -0500

    add favorite indicator to movie listing

--- app/src/main/java/com/jasonmoix/popularmovies/adapters/MovieListingAdapter.java
+++ app/src/main/java/com/jasonmoix/popularmovies/adapters/MovieListingAdapter.java
@@ -9,6 +9,7 @@ import android.widget.CursorAdapter;
 import android.widget.ImageView;
 
 import com.jasonmoix.popularmovies.R;
+import com.jasonmoix.popularmovies.data.MoviesContract;
 import com.jasonmoix.popularmovies.fragments.MovieListingFragment;
 import com.squareup.picasso.Picasso;
 
@@ -22,12 +23,17 @@ public class MovieListingAdapter extends CursorAdapter {
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
 
+        String fav = cursor.getString(cursor.getColumnIndex(MoviesContract.MovieEntry.COLUMN_FAVORITE));
+
         ViewHolder viewHolder = (ViewHolder)view.getTag();
         Picasso.with(context).load(
                 context.getString(R.string.base_movieposter_url,
                         cursor.getString(MovieListingFragment.COL_POSTER_URL)))
                 .fit()
                 .into(viewHolder.poster);
+
+        if(fav.equals("1")) viewHolder.favorite.setVisibility(View.VISIBLE);
+        else viewHolder.favorite.setVisibility(View.INVISIBLE);
 
     }
 
@@ -45,9 +51,11 @@ public class MovieListingAdapter extends CursorAdapter {
     public static class ViewHolder{
 
         public final ImageView poster;
+        public final ImageView favorite;
 
         public ViewHolder(View view){
             poster = (ImageView)view.findViewById(R.id.movie_poster);
+            favorite = (ImageView)view.findViewById(R.id.fav_indicator);
         }
     }
 }
